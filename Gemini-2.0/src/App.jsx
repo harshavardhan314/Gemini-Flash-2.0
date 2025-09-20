17dc689868297966628c66ce60ec0d730e8898ce
harshavardhan314 <hharshavardhan974@gmail.com>
2025-09-20 15:43:35 +0530

    Update App.jsx

--- Gemini-2.0/src/App.jsx
+++ Gemini-2.0/src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Sidebar from './components/sidebar/sidebar'
+import Main from './components/main/main'
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Sidebar></Sidebar>
+    <Main></Main>
+    </>
   )
 }
 
